6ebc77186094f2bab4843f15194a48860bd52d59
ByronMoreno <byron.moreno.dlu@hotmail.com>
2023-06-21 15:03:40 -0500

    Update database.sql
    Actualizacion de trabla de base de datos

--- users/database/database.sql
+++ users/database/database.sql
@@ -1,18 +1,9 @@
 -- Table: public.users
-
--- DROP TABLE IF EXISTS public.users;
-
-CREATE TABLE IF NOT EXISTS public.users
+CREATE TABLE users
 (
-    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    name character varying(255) COLLATE pg_catalog."default",
+    id SERIAL PRIMARY KEY,
+    name character varying(255),
     age integer,
-    description character varying(255) COLLATE pg_catalog."default",
-    created_at timestamp without time zone DEFAULT now(),
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    description character varying(255),
+    created_at timestamp without time zone DEFAULT now()
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.users
-    OWNER to postgres;
